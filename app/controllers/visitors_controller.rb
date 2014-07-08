4deef73207df7e4143e70c1c848900cd414646cc
Elmer Wu <elmerwu@gmail.com>
2014-07-08 11:30:14 -0700

    remove flash and debugger msgs

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,14 +1,14 @@
 class VisitorsController < ApplicationController
 
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
+    # Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
     # rails include this line -> render 'visitors/new'
-    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-    flash[:notice] = "Welcome!"
+    # Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    # flash[:notice] = "Welcome!"
     # flash[:warning] = "This is a flash warning ><"
-    flash[:alert] = "See, we added flash messages!"
-    flash.now[:alert] = "this is a flash.now"
+    # flash[:alert] = "See, we added flash messages!"
+    # flash.now[:alert] = "this is a flash.now"
   end
 
 end
